19df6ba21affd6f79f5f1a3c95ed993608b1beb9
yogacheung <yauka0215@gmail.com>
2019-08-27 00:22:15 -0500

    add login

---
+++ js/login.js
@@ -0,0 +1,22 @@
+function login(name, password, callback){
+  $.ajax({    
+    url: "login.php?name="+name+"&password="+password,    
+  })
+  .done(callback);
+}
+
+///////////////////////////////////////////////////////////
+
+function onlogin() {
+  var gname = document.getElementById("name").value;
+  var gpw = document.getElementById("password").value;
+
+	login(gname, gpw, function(res){
+		  
+		if(res == "no"){
+			alert("Name or/and Password are incorrect!");
+		}else{
+			window.location.href = res;
+		}
+	});
+}
---
+++ login.php
@@ -0,0 +1,21 @@
+<?php 
+	$text = file_get_contents("login.txt");
+	  
+	//echo $text;  
+	  
+	$arr = explode(',', $text);
+	  
+	$name = $_GET["name"];
+	$password = $_GET["password"];
+  
+  //echo $name . " " . $password . "\n";
+  
+  //echo $arr[0] . " " . $arr[1] . "\n";
+    
+	if(($name == $arr[0]) && ($password == $arr[1])) {
+		//var_dump($result);				
+    echo "./timer.html";
+	}else{
+		echo "no";
+	}
+?>
